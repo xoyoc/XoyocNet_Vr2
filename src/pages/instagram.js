95a84d79199c65af4c6b2e9f60e0e619e445c5e4
xoyoc <xoyoc_l2@hotmail.com>
2023-01-08 23:02:02 -0600

    Estilo de card para instagram

--- src/pages/instagram.js
+++ src/pages/instagram.js
@@ -3,15 +3,15 @@ import Layout from "../components/Layout";
 
 const InstagramPage = () => (
   <Layout>
-    <div>
+    <div className="blog-card">
         <div>
             <h1>Antonio Xoyoc Becerra Farias</h1>
         </div>
       <div>
         <p>Vivo en la costa michoacana pasionado Developer, Pilot Drone y Fotografo.</p>
         <div>
-            <a href="tel:7531043367"><button>753 104 33 67</button></a>
-            <a href="mailto:xoyoc_l2@hotmail.com"><button>xoyoc_l2@hotmail.com</button></a>
+            <a href="tel:7531043367"><button className="boton">753 104 33 67</button></a>
+            <a href="mailto:xoyoc_l2@hotmail.com"><button className="boton">xoyoc_l2@hotmail.com</button></a>
         </div>
       </div>
     </div>
